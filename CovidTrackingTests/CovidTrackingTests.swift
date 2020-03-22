71ecb3434df1194e49bb7c594496a6f24bb69bfc
Shawn Bierman <shawnbierman@icloud.com>
2020-03-22 12:53:30 -0500

    Initial Commit

---
+++ CovidTrackingTests/CovidTrackingTests.swift
@@ -0,0 +1,34 @@
+//
+//  CovidTrackingTests.swift
+//  CovidTrackingTests
+//
+//  Created by Shawn Bierman on 3/22/20.
+//  Copyright © 2020 Shawn Bierman. All rights reserved.
+//
+
+import XCTest
+@testable import CovidTracking
+
+class CovidTrackingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
